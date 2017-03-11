1858d1e10b388754e3e09ed6c62a43eb92989488
Shawn Petros <arashcuzi@gmail.com>
2017-03-10 20:45:11 -0600

    yeah, not sure what to call this

--- index.js
+++ index.js
@@ -1,61 +1,51 @@
 'use strict';
-var Alexa = require('alexa-sdk');
+// var Alexa = require('alexa-sdk');
+var Fin = require('financejs');
+var finance = new Fin();
 
-var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-var SKILL_NAME = 'Space Facts';
+// var APP_ID = 'arn:aws:lambda:us-east-1:407197073751:function:FinCalc';
 
-/**
- * Array containing space facts.
- */
-var FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
-];
+// exports.handler = function(event, context, callback) {
+//     var alexa = Alexa.handler(event, context);
+//     alexa.APP_ID = APP_ID;
+//     alexa.registerHandlers(handlers);
+//     alexa.execute();
+// };
+//
+// var handlers = {
+//     'LaunchRequest': function () {
+//         this.emit('GetFact');
+//     },
+//     'GetNewFactIntent': function () {
+//         this.emit('GetFact');
+//     },
+//     'RunCalcs': function () {
+//
+//     },
+//     'AMAZON.HelpIntent': function () {
+//         var speechOutput = "You can say calculate a payment, or how much do I need to save... What can I help you calculate?";
+//         var reprompt = "What can I calculate for you?";
+//         this.emit(':ask', speechOutput, reprompt);
+//     },
+//     'AMAZON.CancelIntent': function () {
+//         this.emit(':tell', 'Goodbye!');
+//     },
+//     'AMAZON.StopIntent': function () {
+//         this.emit(':tell', 'Goodbye!');
+//     }
+// };
 
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
+console.log(finance.AM(20000, 7.5, 5, 0));
+console.log(finance.FV(7, 350, 60));
+let count = 0;
+let int = 0
+let n = 120;
+let c = 350;
+let r = 7;
 
-var handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetFact');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
-    },
-    'GetFact': function () {
-        // Get a random space fact from the space facts list
-        var factIndex = Math.floor(Math.random() * FACTS.length);
-        var randomFact = FACTS[factIndex];
+for (var i = n; i > 0; i--) {
+  int = 1 + (r / 100);
+  count = count * int + c;
+}
 
-        // Create speech output
-        var speechOutput = "Here's your fact: " + randomFact;
-
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
-    },
-    'AMAZON.HelpIntent': function () {
-        var speechOutput = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
-        var reprompt = "What can I help you with?";
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', 'Goodbye!');
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', 'Goodbye!');
-    }
-};
+console.log(`${c} at ${r}% over ${n} periods: `, count / 100);
